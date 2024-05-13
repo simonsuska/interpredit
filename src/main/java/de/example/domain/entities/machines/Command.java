78a546f79ddc77358166a8cbf031bb92d683a691
Simon Suska <simon.suska@proton.me>
2024-05-13 16:43:55 +0200

    #6 - implement RandomAccessMachineCommandTest and RandomAccessMachineDecoderTest

--- src/main/java/de/example/domain/entities/machines/Command.java
+++ src/main/java/de/example/domain/entities/machines/Command.java
@@ -3,5 +3,5 @@ package de.example.domain.entities.machines;
 import de.example.domain.entities.exit.ExitStatus;
 
 public interface Command {
-    ExitStatus execute();
+    ExitStatus execute(Machine machine);
 }
--- src/test/java/de/example/domain/entities/machines/ram/RandomAccessMachineDecoderTest.java
+++ src/test/java/de/example/domain/entities/machines/ram/RandomAccessMachineDecoderTest.java
@@ -1,22 +1,34 @@
 package de.example.domain.entities.machines.ram;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import de.example.domain.entities.exit.ExitStatus;
+import de.example.domain.entities.machines.Command;
+import io.vavr.control.Either;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RandomAccessMachineDecoderTest {
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
     @Test
     void decode() {
+        RandomAccessMachineDecoder decoder = new RandomAccessMachineDecoder();
+        Either<ExitStatus, Command> result;
+
+        result = decoder.decode(null);
+        assertTrue(result.isLeft());
+
+        result = decoder.decode("");
+        assertTrue(result.isLeft());
+
+        result = decoder.decode("CMD");
+        assertTrue(result.isLeft());
+
+        result = decoder.decode("CMD A");
+        assertTrue(result.isLeft());
+
+        result = decoder.decode("CMD 1");
+        assertTrue(result.isRight());
+
+        result = decoder.decode(" CMD 1  ");
+        assertTrue(result.isRight());
     }
 }
